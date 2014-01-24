fcfbf7da2e881aa5c2e8fb92bfcbb4a5ed09a829
Christopher Nelson <nadiasvertex@gmail.com>
2014-01-24 06:27:33 -0500

    Started configuration reactor.

---
+++ src/configuration/reactor.h
@@ -0,0 +1,19 @@
+/*
+ * reactor.h
+ *
+ *  Created on: Jan 23, 2014
+ *      Author: Christopher Nelson
+ */
+
+#ifndef INFINISQLREACTOR_H_
+#define INFINISQLREACTOR_H_
+
+namespace configuration {
+
+class reactor {
+
+};
+
+}
+
+#endif /* INFINISQLREACTOR_H_ */
